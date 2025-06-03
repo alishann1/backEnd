34ca34449af9723efec9f1ab1e690d81d3983506
Ali Shan <alishanamin125@gmail.com>
2025-06-03 17:00:37 +0500

    sas

--- sas/backend/src/routes/owner/owner.route.js
+++ sas/backend/src/routes/owner/owner.route.js
@@ -1,5 +1,5 @@
 import Router from "express";
-import { addPrincipal, imageUpload, login, logout, me, refresh, registerOwner, resendOtp, verifyOtp } from "../../controllers/owner/owner.controller.js"
+import { addPrincipal, getAll, getUser, imageUpload, login, logout, me, refresh, registerOwner, resendOtp, verifyOtp } from "../../controllers/owner/owner.controller.js"
 import upload from "../../middlewares/multer.middleware.js";
 import verifyToken from "../../middlewares/verifyToken.middleware.js";
 import isAllow from "../../middlewares/isAllow.middleware.js";
@@ -14,7 +14,11 @@ router.route("/me").get(verifyToken, me)
 router.route("/upload").post(upload.single("profile"), imageUpload)
 router.route("/refresh").get(refresh)
 router.route("/logout").get(verifyToken, logout)
+router.route("/get-user/:userId").get(getUser)
+router.route("/all").get(getAll)
 router.route("/add-principal").post(verifyToken, isAllow("OWNER"), addPrincipal) //["OWNER" ,  "TEACHER"]
-
+// req
+// file :{ }
 export default router
 
+// async  --> fn
